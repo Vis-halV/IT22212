695a4c0b153e0381c25fff97356d1764115ea76e
Vis-halV <168957641+Vis-halV@users.noreply.github.com>
2024-05-25 10:36:51 +0530

    Update Expt-10.py

--- Expt-10.py
+++ Expt-10.py
@@ -13,19 +13,20 @@ the add function from the calc module to calculate this sum.
 Step 5. Format the sum into the printed string.
 Step 6. Print a string indicating the ASCII form of a and b, and use
 the stingf function from the sting module to calculate this form.
-Step 7. Format the ASCII form into the printed string and end the program."""
-
-
-
+Step 7. Format the ASCII form into the printed string and end the program.
+"""
 
 import cal.calc
 import cal.sting
+import cal
+
 class number:
-def init (self,a,b):
-self.a=a
-self.b=b
-a=int(input('Enter the value of a: '))
-b=int(input('Enter the value of b: '))
-num1=number(a,b)
-print('The Sum Of A and B is:{}'.format(cal.calc.add(num1.a,num1.b)))
-print('The ISCII form of A and B is:{}'.format(cal.sting.stingf(num1)))
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+a = int(input('Enter the value of a: '))
+b = int(input('Enter the value of b: '))
+num1 = number(a, b)
+print('The Sum Of A and B is: {}'.format(cal.calc.add(num1.a, num1.b)))
+print('The ASCII form of A and B is: {}'.format(cal.sting.stingf(num1)))
